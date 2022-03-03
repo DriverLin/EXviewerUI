d78e5e8fe6e6cf029bbc88213b93a13addbfdaaf
DriverLin <lty6531600@outlook.com>
2022-03-03 22:46:44 +0800

    update some scripts

--- makeTranslate.py
+++ makeTranslate.py
@@ -3,33 +3,12 @@ from os import times
 import requests
 
 
-def download_newleast(releaseUrl):
-    try:
-        print("fetching release from ", releaseUrl)
-        result = requests.get(releaseUrl).json()
-        print("fetching release done")
-        for asset in result["assets"]:
-            name = asset["name"]
-            if name == "db.text.json":
-                print("Downloading " + name + " from " + asset["browser_download_url"])
-                file = requests.get(asset["browser_download_url"])
-                print("Downloaded " + name + " successfully")
-                return file.json()
-        return None
-    except Exception as e:
-        print(e)
-        return None
-
-
-jsdata = download_newleast(
-    "https://api.github.com/repos/EhTagTranslation/Database/releases/latest"
-)
-if jsdata is None:
-    print("Failed to download newleast")
-    exit(1)
-
+try:
+    jsdata = json.loads(requests.get("https://github.com/EhTagTranslation/Database/releases/latest/download/db.text.json").text)
+except Exception as e:
+        print("Failed to download newleast",e)
+        exit(1)
 outDict = {}
-
 
 for item in jsdata["data"]:
     tagtype = item["namespace"]
@@ -38,7 +17,8 @@ for item in jsdata["data"]:
         value = item["data"][key]["name"]
         outDict[tagtype][key] = value
 
-js = """const preciseQuery = [[[JSONRESUT]]]
+js = """//using data from https://github.com/EhTagTranslation/Database
+const preciseQuery = [[[JSONRESUT]]]
 
 export default function GetTranslate(type,value) {
     try { 
@@ -74,7 +54,7 @@ export function getGuess(value) {
 )
 
 with open(
-    r"D:\refacted_exviewer\src\components\GetTranslate.js", "w", encoding="UTF-8"
+    r"C:\Users\lty65\projects\ExviewerUI\src\components\GetTranslate.js", "w", encoding="UTF-8"
 ) as f:
     f.write(js)
 print("Successfully write to file")
