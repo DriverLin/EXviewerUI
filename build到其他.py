8bc93cab22a2e6d58ec3e73137c4c878b0ca3202
DriverLin <lty6531600@outlook.com>
2022-03-03 22:46:44 +0800

    update some scripts

--- build到其他.py
+++ build到其他.py
@@ -3,17 +3,17 @@ import shutil
 
 
 shutil.rmtree(r'D:\REF_waterFallDemo')
-shutil.copytree(r"D:\refacted_exviewer\build",r'D:\REF_waterFallDemo')
+shutil.copytree(r"C:\Users\lty65\projects\ExviewerUI\build",r'D:\REF_waterFallDemo')
 with open(r"D:\REF_waterFallDemo\scripts\serverSideConfigure.js",'w') as f:
     f.write('window.serverSideConfigure={type: "Data.db"}')
 
 shutil.rmtree(r"D:\EXviewer\server")
-shutil.copytree(r"D:\refacted_exviewer\build",r"D:\EXviewer\server")
+shutil.copytree(r"C:\Users\lty65\projects\ExviewerUI\build",r"D:\EXviewer\server")
 
 
 
 shutil.rmtree(r"D:\refacted_exviewer\server\server")
-shutil.copytree(r"D:\refacted_exviewer\build",r"D:\refacted_exviewer\server\server")
+shutil.copytree(r"C:\Users\lty65\projects\ExviewerUI\build",r"D:\refacted_exviewer\server\server")
 
 
 print('copy to other done')
